68e2efc66e1f6048f7a693f8ab8bb01a7a9c6c77
modar_algayal <mudar.algayal@helsinki.fi>
2026-07-20 16:57:06 +0300

    Remove visualize import (not needed, avoids matplotlib dependency)

--- src/components/__init__.py
+++ src/components/__init__.py
@@ -3,6 +3,5 @@ from . import build_model
 from . import embeddings
 from . import input as inp
 from . import print_res
-from . import visualize
 from . import build_faiss_model
 from . import get_products
